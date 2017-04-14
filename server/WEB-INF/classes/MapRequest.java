8fcdb7272a4152b2379702d13e0a22fe61564385
Brian Bauman <bauma2bm@cmich.edu>
2017-04-14 01:39:21 -0400

    Implementing MySQL call in MapRequest HTTP servelet

--- server/WEB-INF/classes/MapRequest.java
+++ server/WEB-INF/classes/MapRequest.java
@@ -11,27 +11,43 @@ public class HelloServlet extends HttpServlet {
    @Override
    public void doGet(HttpServletRequest request, HttpServletResponse response)
          throws IOException, ServletException {
+
+      SQLWrapper r = new SQLWrapper();
+      JsonObject j = new JsonObject();
+
+      String northeast = request.getParameter("northeast");
+      String southwest = request.getParameter("southwest");
+      String location  = request.getParameter("location");
+      String[] northeastarr;
+      String[] southwestarr;
+      String[] locationarr;
+      Double[] northeastdbl;
+      Double[] southwestdbl;
+      Double[] locationdbl;
+
+      if (northeast.contains(",") && southwest.contains(",") && location.contains(",")) {
+         northeastarr = northeast.split(",");
+         southwestarr = southwest.split(",");
+         locationarr  = location.split(",");
+
+         northeastdbl = {Double.parseDouble(northeastarr[0]),Double.parseDouble(northeastarr[1])};
+         southwestdbl = {Double.parseDouble(southwestarr[0]),Double.parseDouble(southwestarr[1])};
+         locationdbl = {Double.parseDouble(locationarr[0]),Double.parseDouble(locationarr[1])};
+
+         r.setBounds(northeastdbl, southwestdbl, locationdbl);
+
+         j = r.runQuery();
+      }
  
       // Set the response MIME type of the response message
-      response.setContentType("text/html");
+      response.setContentType("application/json");
       // Allocate a output writer to write the response message into the network socket
       PrintWriter out = response.getWriter();
  
       // Write the response message, in an HTML page
       try {
-         out.println("<html>");
-         out.println("<head><title>Hello, World</title></head>");
-         out.println("<body>");
-         out.println("<h1>Hello, world!</h1>");  // says Hello
-         // Echo client's request information
-         out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
-         out.println("<p>Protocol: " + request.getProtocol() + "</p>");
-         out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
-         out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
-         // Generate a random number upon each request
-         out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
-         out.println("<p>UNPLUGGED</p>");
-         out.println("</body></html>");
+         out.print(j)
+         out.flush();
       } finally {
          out.close();  // Always close the output writer
       }
--- server/WEB-INF/classes/SQLWrapper.java
+++ server/WEB-INF/classes/SQLWrapper.java
@@ -1,13 +1,14 @@
 package UD_mapRequest;
 
 import java.sql.*;
+import javax.json.*;
 
 public class SQLWrapper {
 	
-	private String driver;
-	private String dbUrl;
-	private String user; // Need to encrypt this info at some point
-	private String pass; // REALLY need to encrypt this
+	private String driver = "com.mysql.jdbc.Driver";
+	private String dbUrl = "jdbc:mysql://localhost";
+	private String user = "root"; // Need to encrypt this info at some point
+	private String pass = "unplugged"; // REALLY need to encrypt this
 	public boolean valid = true;
 	private double[] upperleftbd  = {0., 0.};
 	private double[] lowerrightbd = {0., 0.};
@@ -15,7 +16,7 @@ public class SQLWrapper {
 
 	public SQLWrapper() {
 		try {
-			Class.forName(driver);
+			Class.forName(driver).newInstance();
 		} catch (Exception e) {
 			this.valid = false;
 			System.err.println("Invalid driver defined for SQL connection (FATAL)");
@@ -23,27 +24,80 @@ public class SQLWrapper {
 		}
 	}
 
-	public void setBounds(double[] topleft, double[] botright, double[] location) {
+	public void setBounds(double[] northeast, double[] southwest, double[] location) {
 		if (location.length != 2) {
 			System.err.println("Invalid array sent for location");
 			System.err.println("Expected array of length 2, got length " + location.length);
 			return;
 		}
-		else if (topleft.length != 2 || botright.length != 2) {
+		else if (northeast.length != 2 || southwest.length != 2) {
                         System.err.println("Invalid array sent for scrnSize");
                         System.err.println("Expected array of length 2, got length " + scrnSize.length);
                         return;
                 }
 
-		System.arraycopy(topleft, 0, upperleftbd, 0, 2);
-		System.arraycopy(botright, 0, lowerrightbd, 0, 2);
+		System.arraycopy(northeast, 0, upperleftbd, 0, 2);
+		System.arraycopy(southwest, 0, lowerrightbd, 0, 2);
 		System.arraycopy(location, 0, latlong, 0, 2);
 
 		return;
 	}
 
-	private Connection getConnection() throws SQLException {
-		Connection con = null;
-		Properties
-	} 
+	public JsonObject runQuery() {
+		JsonArrayBuilder arrBuilder = Json.createArrayBuilder();
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+		    conn = DriverManager.getConnection(dbUrl + "?user=" this.user + "&password=" + this.pass);
+		} catch (SQLException ex) {
+		    // handle any errors
+		    System.err.println("SQLException: " + ex.getMessage());
+		    System.err.println("SQLState: " + ex.getSQLState());
+		    System.err.println("VendorError: " + ex.getErrorCode());
+		}
+
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(this.getQuery());
+
+			while (rs.next()) {
+				arrBuilder.add(Json.createObjectBuilder().
+					add("id", rs.getInt("id")).
+					add("date", rs.getDate("date")).
+					add("venue", rs.getString("venue")).
+					add("artist", rs.getString("artist")).
+					add("latitude", rs.getFloat("latitude")).
+					add("longitude", rs.getFloat("longitude"))
+				);
+			}
+		}
+		catch (SQLException ex){
+			// handle any errors
+			System.err.println("SQLException: " + ex.getMessage());
+			System.err.println("SQLState: " + ex.getSQLState());
+			System.err.println("VendorError: " + ex.getErrorCode());
+		}
+		finally {
+			if (rs != null) {
+			    try {
+			        rs.close();
+			    } catch (SQLException sqlEx) { }
+			    rs = null;
+			}
+			if (stmt != null) {
+			    try {
+			        stmt.close();
+			    } catch (SQLException sqlEx) { }
+			    stmt = null;
+			}
+		}
+		return arrBuilder.build();
+	}
+
+	private String getQuery() {
+		return "select * from performances left join venues on performances.venue_id = venues.id left join artists on performances.artist_id = artists.id
+			where latitude between " + upperleftbd[0] + " and " + lowerrightbd[0]
+			+ " and longitude between " + upperleftbd[1] + " and " + lowerrightbd[1] + ";";
+	}
 }
